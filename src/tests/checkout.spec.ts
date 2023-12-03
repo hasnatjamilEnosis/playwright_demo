49f8222c0b6f03936d5456f3b4d9d9699b6e889f
jamil2018 <hasnatjamil2018@gmail.com>
2023-12-04 02:22:12 +0600

    changese: implement login steps

---
+++ src/tests/checkout.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test'
+
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/')
+
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/)
+// })
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/')
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click()
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole('heading', { name: 'Installation' })
+//   ).toBeVisible()
+// })
+
+test.describe('Shopping cart validation', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com')
+  })
+
+  test('validate the shopping cart functionality', async ({ page }) => {
+    const loginLogo = page.locator('.login_logo')
+    await expect(loginLogo).toHaveText('Swag Labs')
+    const userNameField = page.locator('#user-name')
+    await userNameField.fill('standard_user')
+    const passwordField = page.locator('#password')
+    await passwordField.fill('secret_sauce')
+    const loginBtn = page.locator('#login-button')
+    await loginBtn.click()
+  })
+})
